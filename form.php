091bb3ac45359831f05b758704f503c5172970d2
Adammzln <43994149+Adammzln@users.noreply.github.com>
2018-10-27 23:30:07 +0800

    Update form.php

--- form.php
+++ form.php
@@ -32,7 +32,7 @@
 		</div>
 		<div class="divider"></div>
 		<ul class="nav menu">
-			<li class="active"><a href="index.html"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
+			<li class="active"><a href="index.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
 			<li class="parent "><a data-toggle="collapse" href="#sub-item-1">
 				<em class="fa fa-navicon">&nbsp;</em> Plants <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
 				</a>
@@ -57,13 +57,13 @@
 				<li><a href="#">
 					<em class="fa fa-home"></em>
 				</a></li>
-				<li class="active">Dashboard</li>
+				<li class="active">Plants</li>
 			</ol>
 		</div><!--/.row-->
 		
 		<div class="row">
 			<div class="col-lg-12">
-				<h1 class="page-header">Dashboard</h1>
+				<h1 class="page-header">Mustard</h1>
 			</div>
 		</div><!--/.row-->
        <div class="background">
@@ -84,46 +84,32 @@
       <div class="screen-body">
         <div class="screen-body-item left">
           <div class="app-title">
-            <span>CONTACT</span>
-            <span>US</span>
+            <span>CUSTOMIZE</span>
+            <span>YOURSELF</span>
           </div>
-          <div class="app-contact">CONTACT INFO : +62 81 314 928 595</div>
+          <div class="app-contact">CONTACT INFO : adammzln4297@gmail.com</div>
         </div>
         <div class="screen-body-item">
           <div class="app-form">
             <div class="app-form-group">
-              <input class="app-form-control" placeholder="NAME" value="Krisantus Wanandi">
+              <input class="app-form-control" placeholder="TEMPERATURE" value="24">
             </div>
             <div class="app-form-group">
-              <input class="app-form-control" placeholder="EMAIL">
+              <input class="app-form-control" placeholder="WATER LEVEL" value="8.67">
             </div>
             <div class="app-form-group">
-              <input class="app-form-control" placeholder="CONTACT NO">
+              <input class="app-form-control" placeholder="HUMIDITY" value="88">
             </div>
-            <div class="app-form-group message">
-              <input class="app-form-control" placeholder="MESSAGE">
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="PH VALUE" value="6.5">
             </div>
             <div class="app-form-group buttons">
               <button class="app-form-button">CANCEL</button>
-              <button class="app-form-button">SEND</button>
+              <button class="app-form-button">CONFIRM</button>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="credits">
-      inspired by
-      <a class="credits-link" href="https://dribbble.com/shots/2666271-Contact" target="_blank">
-        <svg class="dribbble" viewBox="0 0 200 200">
-          <g stroke="#ffffff" fill="none">
-            <circle cx="100" cy="100" r="90" stroke-width="20"></circle>
-            <path d="M62.737004,13.7923523 C105.08055,51.0454853 135.018754,126.906957 141.768278,182.963345" stroke-width="20"></path>
-            <path d="M10.3787186,87.7261455 C41.7092324,90.9577894 125.850356,86.5317271 163.474536,38.7920951" stroke-width="20"></path>
-            <path d="M41.3611549,163.928627 C62.9207607,117.659048 137.020642,86.7137169 189.041451,107.858103" stroke-width="20"></path>
-          </g>
-        </svg>
-        Gururaj
-      </a>
     </div>
   </div>
 </div>
