e0984f27867b3834aebc04b5c2b26c6079050170
Adammzln <43994149+Adammzln@users.noreply.github.com>
2018-10-27 22:45:58 +0800

    Update form.php

--- form.php
+++ form.php
@@ -6,7 +6,7 @@
         Temperature: <input type="text" name="temperature"><br>
         PH Value: <input type="text" name="phvalue"><br>
         Humidity: <input type="text" name="humidity"<br>
-        water Level: <input type="text" name="waterlevel"><br> 
+        Water Level: <input type="text" name="waterlevel"><br> 
         <input type="submit">
     </form>
 
