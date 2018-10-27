fcfa377abb7318d8ba9e712396103a05809f4a3c
Adammzln <43994149+Adammzln@users.noreply.github.com>
2018-10-27 17:36:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -140,7 +140,7 @@
 		</div><!--/.row-->
 			<div class="row">
 				<center>
-					<iframe width="640px" height="480px" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/611233/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+					<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/611233/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line"></iframe>
 				</center>
 			</div>
 		<div class="row">
