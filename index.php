13eb284976b6654ab6a320f98689fbcbdcf85610
Adammzln <43994149+Adammzln@users.noreply.github.com>
2018-10-28 00:49:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -41,13 +41,16 @@
 		</div>
 		<div class="divider"></div>
 		<ul class="nav menu">
-			<li class="active"><a href="index.html"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
+			<li class="active"><a href="index.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
 			<li class="parent "><a data-toggle="collapse" href="#sub-item-1">
 				<em class="fa fa-navicon">&nbsp;</em> Plants <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
 				</a>
 				<ul class="children collapse" id="sub-item-1">
 					<li><a class="" href="form.php">
 						<span class="fa fa-arrow-right">&nbsp;</span> Lettuce
+							<ul><li>
+								<span class="fa fa-arrow-right">&nbsp;</span> Lettuce
+							</li></ul>	
 					</a></li>
 					<li><a class="" href="#">
 						<span class="fa fa-arrow-right">&nbsp;</span> Sub Item 2
