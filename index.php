46beada2c28b2f4d01d30a0f9b4446007f7292b8
Adammzln <43994149+Adammzln@users.noreply.github.com>
2018-10-28 01:54:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -101,7 +101,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md1-2">
+			<div class="col-md-12">
 				<div class="panel panel-default">
 					<div class="panel-heading">
 						Temperature
