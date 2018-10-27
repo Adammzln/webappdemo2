71b31a38aa82bc103c89fdb2712b0817be824b41
Adammzln <43994149+Adammzln@users.noreply.github.com>
2018-10-27 22:10:11 +0800

    Add files via upload

---
+++ form.php
@@ -0,0 +1,15 @@
+<? php
+    import urllib
+    import urllib2
+
+        url = 'http://www.someserver.com/cgi-bin/register.cgi'
+        values = {'name' : 'Michael Foord',
+                  'location' : 'Northampton',
+                  'language' : 'Python' }
+
+        data = urllib.urlencode(values)
+        req = urllib2.Request(url, data)
+        response = urllib2.urlopen(req) 
+        the_page = response.read()
+            
+?>
