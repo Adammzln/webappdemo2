0539deac9253bebfe114ef66a171203fda241bb4
Adammzln <43994149+Adammzln@users.noreply.github.com>
2018-10-27 18:15:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -206,7 +206,7 @@
 						<span class="pull-right clickable panel-toggle panel-button-tab-left"><em class="fa fa-toggle-up"></em></span></div>
 					<div class="panel-body">
 						<div class="canvas-wrapper">
-							<canvas class="main-chart" id="line-chart" height="200" width="600"></canvas>
+							
 						</div>
 					</div>
 				</div>
