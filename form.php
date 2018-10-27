18f70d9791fba295c4f066d48712e9db98eadb4a
Adammzln <43994149+Adammzln@users.noreply.github.com>
2018-10-28 01:14:52 +0800

    Update form.php

--- form.php
+++ form.php
@@ -25,13 +25,6 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span></button>
 				<a class="navbar-brand" href="#"><span>SUFECS</span>Admin</a>
-				<ul class="nav navbar-top-links navbar-right">
-					<li class="dropdown"><a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-						<em class="fa fa-envelope"></em><span class="label label-danger">15</span>
-					</a>	
-
-					</li>
-				</ul>
 			</div>
 		</div><!-- /.container-fluid -->
 	</nav>
