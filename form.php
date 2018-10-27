0a73b5764670301b2d6e523dd1b3a229e368bab0
Adammzln <43994149+Adammzln@users.noreply.github.com>
2018-10-28 01:08:07 +0800

    Update form.php

--- form.php
+++ form.php
@@ -28,54 +28,8 @@
 				<ul class="nav navbar-top-links navbar-right">
 					<li class="dropdown"><a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
 						<em class="fa fa-envelope"></em><span class="label label-danger">15</span>
-					</a>
-						<ul class="dropdown-menu dropdown-messages">
-							<li>
-								<div class="dropdown-messages-box"><a href="profile.html" class="pull-left">
-									<img alt="image" class="img-circle" src="http://placehold.it/40/30a5ff/fff">
-									</a>
-									<div class="message-body"><small class="pull-right">3 mins ago</small>
-										<a href="#"><strong>John Doe</strong> commented on <strong>your photo</strong>.</a>
-									<br /><small class="text-muted">1:24 pm - 25/03/2015</small></div>
-								</div>
-							</li>
-							<li class="divider"></li>
-							<li>
-								<div class="dropdown-messages-box"><a href="profile.html" class="pull-left">
-									<img alt="image" class="img-circle" src="http://placehold.it/40/30a5ff/fff">
-									</a>
-									<div class="message-body"><small class="pull-right">1 hour ago</small>
-										<a href="#">New message from <strong>Jane Doe</strong>.</a>
-									<br /><small class="text-muted">12:27 pm - 25/03/2015</small></div>
-								</div>
-							</li>
-							<li class="divider"></li>
-							<li>
-								<div class="all-button"><a href="#">
-									<em class="fa fa-inbox"></em> <strong>All Messages</strong>
-								</a></div>
-							</li>
-						</ul>
-					</li>
-					<li class="dropdown"><a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-						<em class="fa fa-bell"></em><span class="label label-info">5</span>
-					</a>
-						<ul class="dropdown-menu dropdown-alerts">
-							<li><a href="#">
-								<div><em class="fa fa-envelope"></em> 1 New Message
-									<span class="pull-right text-muted small">3 mins ago</span></div>
-							</a></li>
-							<li class="divider"></li>
-							<li><a href="#">
-								<div><em class="fa fa-heart"></em> 12 New Likes
-									<span class="pull-right text-muted small">4 mins ago</span></div>
-							</a></li>
-							<li class="divider"></li>
-							<li><a href="#">
-								<div><em class="fa fa-user"></em> 5 New Followers
-									<span class="pull-right text-muted small">4 mins ago</span></div>
-							</a></li>
-						</ul>
+					</a>	
+
 					</li>
 				</ul>
 			</div>
@@ -100,7 +54,7 @@
 				</a>
 				<ul class="children collapse" id="sub-item-1">
 					<li><a class="" href="form.php">
-						<span class="fa fa-arrow-right">&nbsp;</span> Mustard
+						<span class="fa fa-arrow-right">&nbsp;</span> Lettuce
 					</a></li>
 					<li><a class="" href="#">
 						<span class="fa fa-arrow-right">&nbsp;</span> Sub Item 2
@@ -125,7 +79,7 @@
 		
 		<div class="row">
 			<div class="col-lg-12">
-				<h1 class="page-header">Mustard</h1>
+				<h1 class="page-header">Lettuce</h1>
 			</div>
 		</div><!--/.row-->
        <div class="background">
@@ -154,16 +108,16 @@
         <div class="screen-body-item">
           <div class="app-form">
             <div class="app-form-group">
-              <input class="app-form-control" placeholder="TEMPERATURE" value="TEMPERATURE: 24">
+              <input class="app-form-control" placeholder="TEMPERATURE(°C): 24">
             </div>
             <div class="app-form-group">
-              <input class="app-form-control" placeholder="WATER LEVEL" value="WATER LEVEL: 8.67">
+              <input class="app-form-control" placeholder="WATER LEVEL(cm): 8.67" value="WATER LEVEL: 8.67">
             </div>
             <div class="app-form-group">
-              <input class="app-form-control" placeholder="HUMIDITY" value="HUMIDITY: 88">
+              <input class="app-form-control" placeholder="HUMIDITY(%): 88" value="HUMIDITY: 88">
             </div>
             <div class="app-form-group">
-              <input class="app-form-control" placeholder="PH VALUE" value="PH VALUE: 6.5">
+              <input class="app-form-control" placeholder="PH VALUE: 6.5" value="PH VALUE: 6.5">
             </div>
             <div class="app-form-group buttons">
               <button class="app-form-button">CANCEL</button>
