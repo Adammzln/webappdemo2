c2b0b940ed6469736c80c160e48c1fce80109bbc
Adammzln <43994149+Adammzln@users.noreply.github.com>
2018-10-27 23:22:29 +0800

    Update form.php

--- form.php
+++ form.php
@@ -1,7 +1,20 @@
 <!DOCTYPE html>
    <html>
       <head>
-         <link href="css/styles.css" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>SmartFarm</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/datepicker3.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
+	
+	<!--Custom Font-->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.js"></script>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
       </head>
     <body>
        	<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
